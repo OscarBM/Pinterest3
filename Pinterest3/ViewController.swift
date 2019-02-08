2a098d6a3b581f07571306d27ec5ed997eda942a
Oscar <oscar@oscars-imac.local>
2019-02-07 21:59:48 -0800

    Initial Commit

---
+++ Pinterest3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pinterest3
+//
+//  Created by Oscar on 2/7/19.
+//  Copyright © 2019 Oscar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
