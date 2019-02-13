1bb2bb3a3ed592f0b346ef4c6f2c217d0d1347bb
Oscar <oscar@Oscars-iMac.local>
2019-02-12 21:36:27 -0800

    pods listo

---
+++ Pods/Target Support Files/Pods-Pinterest3/Pods-Pinterest3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pinterest3 : NSObject
+@end
+@implementation PodsDummy_Pods_Pinterest3
+@end
---
+++ Pods/Target Support Files/Pods-Pinterest3Tests/Pods-Pinterest3Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pinterest3Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Pinterest3Tests
+@end
